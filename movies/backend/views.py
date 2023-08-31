f3cfad9c79b549569266f91e00e599a2f1e7cc87
Name <shi-666@yandex.ru>
2023-08-31 20:11:13 +0300

    first view

--- movies/backend/views.py
+++ movies/backend/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic.base import View
 
-# Create your views here.
+
+class MoviesView(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movies/movie_list.html', {'movie_list': movies})
+    
+    
